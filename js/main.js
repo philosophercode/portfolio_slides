56d8d2f1da14425358a497039cc32e5350df9ea9
Isaac S <philosophercode@gmail.com>
2018-06-21 01:03:44 -0400

    added fullpagejs and font Lato

---
+++ js/main.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+	$('#fullpage').fullpage({
+		anchors: ['page1', 'page2', 'page3', 'page4'],
+		menu: '#navbar',
+		controlArrows: false,
+		slidesNavigation: true,
+		// slidesNavPosition: 'top',
+		// fadingEffect: true,
+		// paddingBottom: '30px'
+	});
+});
