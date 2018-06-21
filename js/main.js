61ac55cfb576fe18fb27dd5045a670c5e9549ffd
Isaac S <philosophercode@gmail.com>
2018-06-21 01:34:49 -0400

    centered slides nav bar dots

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
 	$('#fullpage').fullpage({
 		anchors: ['page1', 'page2', 'page3', 'page4'],
 		menu: '#navbar',
-		controlArrows: false,
+		// controlArrows: false,
 		slidesNavigation: true,
 		// slidesNavPosition: 'top',
 		// fadingEffect: true,
